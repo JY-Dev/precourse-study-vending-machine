353fcef86a6d7d5b498e5a1efd328a767a43a470
jaeyoung <kjy0302014@gmail.com>
2022-12-28 16:03:55 +0900

    Coin amountToCoin 함수 생성 및 테스트 코드 작성

--- src/main/kotlin/vendingmachine/domain/coin/model/Coin.kt
+++ src/main/kotlin/vendingmachine/domain/coin/model/Coin.kt
@@ -1,5 +1,8 @@
 package vendingmachine.domain.coin.model
 
+import vendingmachine.excpetion.convertExceptionMessage
+import java.lang.IllegalArgumentException
+
 enum class Coin(private val amount: Int) {
     COIN_500(500),
     COIN_100(100),
@@ -12,6 +15,10 @@ enum class Coin(private val amount: Int) {
         }
         fun getCoinsAmount(predicate : (amount : Int) -> Boolean) : List<Int>{
             return values().map { it.amount }.filter(predicate)
+        }
+        fun amountToCoin(amount : Int) : Coin{
+            return values().find { it.amount == amount } ?:
+                throw IllegalArgumentException(convertExceptionMessage("Amount에 맞는 Coin이 없습니다."))
         }
     }
 }
--- src/test/kotlin/vendingmachine/coin/CoinTest.kt
+++ src/test/kotlin/vendingmachine/coin/CoinTest.kt
@@ -2,7 +2,9 @@ package vendingmachine.coin
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import vendingmachine.domain.coin.model.Coin
+import java.lang.IllegalArgumentException
 
 class CoinTest {
 
@@ -14,5 +16,17 @@ class CoinTest {
     @Test
     fun `getCoinsAmount predicate를 적용한 return 값은 조건에 맞게 필터링 되야 한다`(){
         Assertions.assertThat(Coin.getCoinsAmount { it > 500 }.size).isEqualTo(0)
+    }
+
+    @Test
+    fun `amountToCoin의 결과 값은 Coin amount에 따른 Coin를 반환해야한다`(){
+        val amount = 100
+        Assertions.assertThat(Coin.amountToCoin(amount)).isEqualTo(Coin.COIN_100)
+    }
+
+    @Test
+    fun `amountToCoin의 결과 값은 Coin amount맞지 않으면 IllegalArgumentException이 발생한다`(){
+        val amount = 222
+        assertThrows<IllegalArgumentException> { Coin.amountToCoin(amount) }
     }
 }
