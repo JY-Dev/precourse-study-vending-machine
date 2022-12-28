ead812e14834992d98c1438019cd3d0b61354dd3
jaeyoung <kjy0302014@gmail.com>
2022-12-28 15:25:50 +0900

    CoinChanger changeCoins return 값 List -> Map으로 변경

--- src/main/kotlin/vendingmachine/domain/coin/changer/CoinChanger.kt
+++ src/main/kotlin/vendingmachine/domain/coin/changer/CoinChanger.kt
@@ -1,6 +1,7 @@
 package vendingmachine.domain.coin.changer
 
 import vendingmachine.domain.coin.model.ChangeCoin
+import vendingmachine.domain.coin.model.Coin
 
 /**
  * 금액을 Coin으로 반환하기 위한 interface
@@ -9,7 +10,7 @@ interface CoinChanger {
     /**
      * @param money : 변경하고자 하는 금액
      * 입력받은 금액을 Coin으로 변경하는 함수
-     * @return Coin과 Coin 갯수를 담고있는 ChangeCoin
+     * @return Coin과 Coin 갯수를 담고있는 Map
      */
-    fun changeCoins(money : Int) : List<ChangeCoin>
+    fun changeCoins(money : Int) : Map<Coin,Int>
 }
--- src/main/kotlin/vendingmachine/domain/coin/changer/RandomCoinChanger.kt
+++ src/main/kotlin/vendingmachine/domain/coin/changer/RandomCoinChanger.kt
@@ -1,10 +1,11 @@
 package vendingmachine.domain.coin.changer
 
 import vendingmachine.domain.coin.model.ChangeCoin
+import vendingmachine.domain.coin.model.Coin
 
 class RandomCoinChanger : CoinChanger{
-    override fun changeCoins(money: Int): List<ChangeCoin> {
-
-        return emptyList()
+    override fun changeCoins(money: Int): Map<Coin, Int> {
+        return mapOf()
     }
+
 }
